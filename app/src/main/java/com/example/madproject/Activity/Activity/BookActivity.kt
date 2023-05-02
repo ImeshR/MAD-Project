769c2514c28cffb623aa102c2b1fe349dd03c25a
Eshan <111144332+ImeshR@users.noreply.github.com>
2023-05-02 13:00:46 +0530

    updated

--- app/src/main/java/com/example/madproject/Activity/Activity/BookActivity.kt
+++ app/src/main/java/com/example/madproject/Activity/Activity/BookActivity.kt
@@ -4,10 +4,9 @@ import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.example.madproject.R
 
-class MainActivity : AppCompatActivity() {
+class BookActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_book_categories)
-
     }
 }
