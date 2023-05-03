cf78bf10145156e5ec88eac1b3cdc8ad940d484f
IT21186424 <shehanmalingang2@gmail.com>
2023-05-04 02:27:59 +0530

    create delivery details page

---
+++ app/src/main/java/com/example/madproject/Activity/Activity/Add_delivery_details.kt
@@ -0,0 +1,11 @@
+package com.example.madproject.Activity.Activity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Add_delivery_details : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_add_delivery_details)
+    }
+}
