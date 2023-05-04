7244c6d14e3086978a6c2622a6fd169a6f990df0
Diwan Sachidu <45502921+sachidumaleesha@users.noreply.github.com>
2023-05-04 12:42:41 +0530

    Add Authentication

--- app/src/main/java/com/example/madproject/Activity/Activity/Login.kt
+++ app/src/main/java/com/example/madproject/Activity/Activity/Login.kt
@@ -1,21 +1,49 @@
 package com.example.madproject.Activity.Activity
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import com.example.madproject.R
 import com.example.madproject.databinding.ActivityLoginBinding
+import com.google.firebase.auth.FirebaseAuth
 
 class Login : AppCompatActivity() {
 
     lateinit var binding: ActivityLoginBinding
+    private lateinit var firebaseAuth: FirebaseAuth
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
 
+        firebaseAuth = FirebaseAuth.getInstance()
+
         binding = ActivityLoginBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+
+        binding.loginBtn.setOnClickListener {
+            val email = binding.loginEmail.text.toString()
+            val pass = binding.loginPass.text.toString()
+
+            if (email.isNotEmpty() && pass.isNotEmpty()) {
+                firebaseAuth.signInWithEmailAndPassword(email, pass).addOnCompleteListener {
+                    if (it.isSuccessful) {
+                        Toast.makeText(this, "Login Successful", Toast.LENGTH_SHORT).show()
+                        val intent = Intent(this, MainActivity::class.java)
+                        startActivity(intent)
+                    } else {
+                        Toast.makeText(this, it.exception.toString(), Toast.LENGTH_SHORT).show()
+                    }
+                }
+            } else {
+                Toast.makeText(this, "Empty Fields Are Not Allowed !!!", Toast.LENGTH_SHORT).show()
+            }
+        }
+
+
+        //Intent
         binding.navigateBackLogin.setOnClickListener {
             startActivity(
                 android.content.Intent(this, MainNavigator::class.java)
@@ -25,12 +53,6 @@ class Login : AppCompatActivity() {
         binding.linktoSignUp.setOnClickListener {
             startActivity(
                 android.content.Intent(this, SignUp::class.java)
-            )
-        }
-
-        binding.loginBtn.setOnClickListener {
-            startActivity(
-                android.content.Intent(this, MainActivity::class.java)
             )
         }
     }
--- app/src/main/java/com/example/madproject/Activity/Activity/SignUp.kt
+++ app/src/main/java/com/example/madproject/Activity/Activity/SignUp.kt
@@ -1,13 +1,20 @@
 package com.example.madproject.Activity.Activity
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import com.example.madproject.R
 import com.example.madproject.databinding.ActivitySignUpBinding
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.firestore.auth.User
 
 class SignUp : AppCompatActivity() {
 
     private lateinit var binding: ActivitySignUpBinding
+    private lateinit var firebaseAuth: FirebaseAuth
+    private lateinit var firestore: FirebaseFirestore
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -16,11 +23,33 @@ class SignUp : AppCompatActivity() {
         binding = ActivitySignUpBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        firebaseAuth = FirebaseAuth.getInstance()
+        firestore = FirebaseFirestore.getInstance()
+
+
         binding.signUpBtn.setOnClickListener {
-            startActivity(
-                android.content.Intent(this, Login::class.java)
-            )
+            val firstName = binding.singUpFirstName.text.toString()
+            val lastName = binding.singUpLastName.text.toString()
+            val email = binding.singUpEmail.text.toString()
+            val mobileNumber = binding.singUpNumber.text.toString()
+            val password = binding.singUpPass.text.toString()
+            val confirmPassword = binding.singUpConfirmPass.text.toString()
+
+            if (firstName.isNotEmpty() && lastName.isNotEmpty() && email.isNotEmpty() && mobileNumber.isNotEmpty() && password.isNotEmpty() && confirmPassword.isNotEmpty()) {
+                if (password == (confirmPassword)) {
+                    firebaseAuth.createUserWithEmailAndPassword(email, password).addOnCompleteListener {
+                        saveUser(it.result?.user?.uid.toString(), firstName, lastName, email, mobileNumber, password)
+                    }
+                } else {
+                    Toast.makeText(this, "Password is not matching", Toast.LENGTH_SHORT).show()
+                }
+            } else {
+                Toast.makeText(this, "Empty Fields Are Not Allowed !!!", Toast.LENGTH_SHORT).show()
+            }
         }
+
+
+        //Intent Paths
 
         binding.linktoLogin.setOnClickListener {
             startActivity(
@@ -33,6 +62,19 @@ class SignUp : AppCompatActivity() {
                 android.content.Intent(this, MainNavigator::class.java)
             )
         }
+    }
 
+    private fun saveUser(uid: String, firstName: String, lastName: String, email: String, mobileNumber: String, password: String) {
+        val user = UserData(uid, firstName, lastName, email, mobileNumber, password)
+        firestore.collection("users").document(uid).set(user).addOnCompleteListener {
+            if (it.isSuccessful) {
+                Toast.makeText(this, "User Saved", Toast.LENGTH_SHORT).show()
+                val intent = Intent(this, Login::class.java)
+                intent.putExtra("USER_ID", uid)
+                startActivity(intent)
+            } else {
+                Toast.makeText(this, it.exception.toString(), Toast.LENGTH_SHORT).show()
+            }
+        }
     }
 }
