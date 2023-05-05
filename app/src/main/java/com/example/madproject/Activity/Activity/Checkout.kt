652d3c99db6257e62366b8f07aca88e596638503
IT21183768 <it21183768@my.sliit.lk>
2023-05-06 01:29:22 +0530

    checkout details Box

---
+++ app/src/main/java/com/example/madproject/Activity/Activity/Checkout.kt
@@ -0,0 +1,12 @@
+package com.example.madproject.Activity.Activity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.madproject.R
+
+class Checkout : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_checkout)
+    }
+}
