96d68f81c24d5f54847708f9367f54e7292b79e2
IT21183768 <it21183768@my.sliit.lk>
2023-05-05 23:25:39 +0530

    Revert "create crud"
    This reverts commit 4b3b282b5cc9c3b1eaf931c37b2b29ded590e46f.

--- app/src/main/java/com/example/madproject/Activity/Activity/Card.kt
+++
@@ -1,5 +0,0 @@
-package com.example.madproject.Activity.Activity
-
-data class Card(val name : String? = null, val cardNum: String? = null,val month: String? = null, val year: String? = null, val cvv: String? = null){
-
-}
--- app/src/main/java/com/example/madproject/Activity/Activity/cardAdd_Form.kt
+++ app/src/main/java/com/example/madproject/Activity/Activity/cardAdd_Form.kt
@@ -2,53 +2,42 @@ package com.example.madproject.Activity.Activity
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.Toast
+import androidx.fragment.app.Fragment
 import com.example.madproject.R
-import com.example.madproject.databinding.ActivityCardAddFormBinding
-import com.google.firebase.auth.FirebaseAuth
-import com.google.firebase.database.DatabaseReference
-import com.google.firebase.database.FirebaseDatabase
-import com.google.firebase.firestore.FirebaseFirestore
+import com.example.madproject.databinding.ActivityMainBinding
 
 class cardAdd_Form : AppCompatActivity() {
 
-    private lateinit var binding: ActivityCardAddFormBinding
-    private lateinit var firebaseAuth: FirebaseAuth
-    private lateinit var firestore: FirebaseFirestore
-    private lateinit var database: DatabaseReference
+    private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
-
-        firebaseAuth = FirebaseAuth.getInstance()
-        firestore = FirebaseFirestore.getInstance()
-
         super.onCreate(savedInstanceState)
-        binding = ActivityCardAddFormBinding.inflate(layoutInflater)
+        setContentView(R.layout.activity_card_add_form)
+        binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        replaceFragment(Home())
 
-        binding.submitcardBtn.setOnClickListener{
-            val s_name = binding.holderName.text.toString()
-            val s_cardNum = binding.cardNum.text.toString()
-            val s_month = binding.month.text.toString()
-            val s_year = binding.year.text.toString()
-            val s_cvv = binding.cvv.text.toString()
+        binding.BottmoNavigationView.setOnItemSelectedListener {
+            when (it.itemId) {
+                R.id.home -> replaceFragment(Home())
+                R.id.category -> replaceFragment(Category())
+                R.id.cart -> replaceFragment(Cart())
+                R.id.userprofile -> replaceFragment(Profile())
 
-            // Add code to save card details to the Firebase database
+                else -> {
+                    false
+                }
 
-            database = FirebaseDatabase.getInstance().getReference("Cards")
-            val Card = Card(s_name, s_cardNum, s_month, s_year, s_cvv )
-            database.child(s_name).setValue(Card).addOnSuccessListener {
-                binding.holderName.text.clear()
-                binding.cardNum.text.clear()
-                binding.month.text.clear()
-                binding.year.text.clear()
-
-                Toast.makeText(this, "Successfully Saved", Toast.LENGTH_SHORT).show()
-
-            }.addOnFailureListener{
-                Toast.makeText(this, "Failed", Toast.LENGTH_SHORT).show()
             }
+            true
+
         }
     }
 
+    private fun replaceFragment(fragment: Fragment) {
+        val fragmentManager = supportFragmentManager
+        val fragmentTransaction = fragmentManager.beginTransaction()
+        fragmentTransaction.replace(R.id.frameLayout, fragment)
+        fragmentTransaction.commit()
+    }
 }
