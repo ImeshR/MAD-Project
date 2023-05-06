0febe87bb0283e23dd2e58716508ae4c895bebbf
Diwan Sachidu <45502921+sachidumaleesha@users.noreply.github.com>
2023-05-06 22:13:27 +0530

    Fix some errors

--- app/src/main/java/com/example/madproject/Activity/Activity/Checkout.kt
+++ app/src/main/java/com/example/madproject/Activity/Activity/Checkout.kt
@@ -1,12 +1,27 @@
 package com.example.madproject.Activity.Activity
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.madproject.R
+import com.example.madproject.databinding.ActivityCheckoutBinding
 
 class Checkout : AppCompatActivity() {
+
+    private lateinit var binding: ActivityCheckoutBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_checkout)
+
+        binding = ActivityCheckoutBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+
+        binding.editDilivaryDetails.setOnClickListener {
+            startActivity(
+                Intent(this, UpdateDeliveryDetailsActivity::class.java)
+            )
+        }
     }
 }
