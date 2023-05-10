795aaecf4d2d91faabe3fa96ca170f84240ec15c
IT21186424 <shehanmalingang2@gmail.com>
2023-05-10 17:53:21 +0530

    update checkout

--- app/src/main/java/com/example/madproject/Activity/Activity/Checkout.kt
+++ app/src/main/java/com/example/madproject/Activity/Activity/Checkout.kt
@@ -3,12 +3,15 @@ package com.example.madproject.Activity.Activity
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
 import com.example.madproject.R
 import com.example.madproject.databinding.ActivityCheckoutBinding
 
 class Checkout : AppCompatActivity() {
 
     private lateinit var binding: ActivityCheckoutBinding
+   
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -16,6 +19,18 @@ class Checkout : AppCompatActivity() {
 
         binding = ActivityCheckoutBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+
+
+        val intent = intent
+        val addressString = intent.getStringExtra("address")
+        val zipString = intent.getStringExtra("zip")
+        val cityString = intent.getStringExtra("city")
+        val tpString = intent.getStringExtra("tp")
+
+
+
+
 
 
         binding.editDilivaryDetails.setOnClickListener {
